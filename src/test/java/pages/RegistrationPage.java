450d988e2ea7bc0ec3c0a06fed628fdd7cea92d5
svasenkov <aanher@gmail.com>
2023-09-26 20:15:37 +0200

    added pageObjects

---
+++ src/test/java/pages/RegistrationPage.java
@@ -0,0 +1,66 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+
+public class RegistrationPage {
+
+    ///// SelenideElements
+    SelenideElement titleLabel = $(".practice-form-wrapper"),
+            firstNameInput = $("#firstName"),
+            lastNameInput = $("#lastName"),
+            userEmailInput = $("#userEmail"),
+            genderWrapper = $("#genterWrapper"),
+            userNumberInput = $("#userNumber");
+
+
+    ///// Actions
+    public RegistrationPage openPage() {
+        open("/automation-practice-form");
+        titleLabel.shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        return this;
+    }
+
+
+    public RegistrationPage setFirstName(String value) {
+        firstNameInput.setValue(value);
+
+        return this;
+    }
+
+//    public void clickFirstName() { // not used
+//        firstNameInput.click();
+//    }
+
+    public RegistrationPage setLastName(String value) {
+        lastNameInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setEmail(String value) {
+        userEmailInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setGender(String value) {
+        genderWrapper.$(byText(value)).click();
+
+        return this;
+    }
+
+    public RegistrationPage setUserNumber(String value) {
+        userNumberInput.setValue(value);
+
+        return this;
+    }
+
+}
---
+++ src/test/java/tests/RegistrationWithPageObjectsTests.java
@@ -0,0 +1,44 @@
+package tests;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import pages.RegistrationPage;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationWithPageObjectsTests extends TestBase {
+
+    RegistrationPage registrationPage = new RegistrationPage();
+
+    @Test
+    void successfulRegistrationTest() {
+        registrationPage.openPage()
+                .setFirstName("Alex")
+                .setLastName("Egorov")
+                .setEmail("alex@egorov.com")
+                .setGender("Other")
+                .setUserNumber("1234567890");
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("July");
+        $(".react-datepicker__year-select").selectOption("2008");
+        $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("img/1.png");
+        $("#currentAddress").setValue("Some address 1");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Alex"), text("Egorov"),
+                text("alex@egorov.com"), text("1234567890"));
+    }
+}
