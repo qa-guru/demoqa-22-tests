5a9180cd5117a5765f5f26facdf6d443df9ba0eb
svasenkov <aanher@gmail.com>
2023-09-29 20:39:11 +0200

    added randomutils

---
+++ src/test/java/tests/RegistrationWithRandomUtilsTests.java
@@ -0,0 +1,53 @@
+package tests;
+
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static utils.RandomUtils.getRandomEmail;
+import static utils.RandomUtils.getRandomString;
+
+public class RegistrationWithRandomUtilsTests extends TestBase {
+
+    @Test
+    void successfulRegistrationTest() {
+        String firstName = getRandomString(10);
+        String lastName = getRandomString(10);
+        String userEmail = getRandomEmail();
+        String streetAddress = "";
+
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(userEmail);
+        $("#genterWrapper").$(byText("Other")).click();
+        $("#userNumber").setValue("1234567890");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("July");
+        $(".react-datepicker__year-select").selectOption("2008");
+        $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("img/1.png");
+        $("#currentAddress").setValue(streetAddress);
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text(firstName), text(lastName),
+                text(userEmail), text("1234567890"), text(streetAddress));
+    }
+}
---
+++ src/test/java/utils/RandomUtils.java
@@ -0,0 +1,57 @@
+package utils;
+
+import java.security.SecureRandom;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomUtils {
+
+    public static void main(String[] args) {
+        System.out.println(getRandomString(10));
+        System.out.println(getRandomEmail());
+        System.out.println(getRandomAddress());
+        System.out.println(getRandomInt(111, 99999999));
+        System.out.println(getRandomPhone());
+        System.out.println(getRandomGender());
+    }
+
+    public static String getRandomString(int len) {
+//        String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        String AB = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        SecureRandom rnd = new SecureRandom();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < len; i++)
+            sb.append(AB.charAt(rnd.nextInt(AB.length())));
+
+        return sb.toString();
+    }
+
+    public static String getRandomEmail() {
+        return getRandomString(10) + "@qa.guru";
+    }
+
+    public static String getRandomAddress() {
+        return getRandomString(10) + " " + getRandomString(10) + " " + getRandomString(10);
+    }
+
+    public static int getRandomInt(int min, int max) {
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
+    }
+
+    // +3 (263) 253 - 66 - 12
+    public static String getRandomPhone() {
+        return String.format("+%s (%s) %s - %s - %s", getRandomInt(1, 9), getRandomInt(111, 999),
+                getRandomInt(111, 999), getRandomInt(11, 99), getRandomInt(11, 99));
+    }
+
+    public static String getRandomGender() {
+        String[] genders = {"Male", "Female", "Other"};
+
+        return getRandomItemFromArray(genders);
+    }
+
+    public static String getRandomItemFromArray(String[] array) {
+        int index = getRandomInt(0, array.length - 1);
+
+        return array[index];
+    }
+}
